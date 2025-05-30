3d7fbcf198a2ab6ba79d814332cda0f98db38df0
Ashebir Diriba <147692541+Ashed2127@users.noreply.github.com>
2025-05-31 00:50:41 +0300

    Update tx_ref_verify.js

--- backend/tx_ref_verify.js
+++ backend/tx_ref_verify.js
@@ -4,7 +4,7 @@
 //   method: "POST",
 //   url: "https://api.chapa.co/v1/transaction/verify/ace442dd-ea8c-4920-9219-4b0d28169d4c",
 //   headers: {
-//     Authorization: "Bearer CHASECK_TEST-sAZLjV7jiqT7HF5XBFofZxT5Zz416nCn",
+//     Authorization: "Bearer YOU CHAPA API KEY HERE",
 //   },
 // };
 
